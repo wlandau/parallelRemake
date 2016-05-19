0a052784f3a0f593754c30ea62f9e069b607f427
wlandau <will.landau@gmail.com>
2016-05-19 17:35:42 -0400

    Added another example to the README
    Small scaled-down simulation study that uses automation
    to generate the YAML files for parallel instances of
    remake.

--- R/clean_stages.R
+++ R/clean_stages.R
@@ -1,19 +1,27 @@
-#' @title Function \code{name_stages}
-#' @description Utility function to assign names the \code{stages} argument to 
+#' @title Function \code{clean_stages}
+#' @description Utility function to clean the \code{stages} argument to 
 #' the function \code{remake2make}.
 #' @seealso remake2make
 #' @export
 #' @param stages List of character vectors. Each character vector
 #' is a subcollections of \code{remake} YAML files that can be run in parallel. 
-#' Do not include the .yml extensions of the remake files. For example, if I want to run 
+#' The .yml extensions of the remake files are optional. For example, if I want to run 
 #' remake1.yml and remake2.yml in parallel and then run finish.yml, I would specify
+#'     stages = list(
+#'       c("remake1.yml", "remake2.yml"),
+#'       "finish.yml"
+#'     )
+#' 
+#' or 
+#' 
 #'     stages = list(
 #'       c("remake1", "remake2"),
 #'       "finish"
 #'     )
-name_stages = function(stages){
+clean_stages = function(stages){
   if(is.null(names(stages))) names(stages) = paste0("stage", 1:length(stages))
   i = !nchar(names(stages))
   names(stages)[i] = paste0("unnamed_stage_", 1:sum(i))
+  stages = lapply(stages, gsub, pattern = ".yml$", replacement = "")
   stages
 }
