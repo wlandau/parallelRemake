7f73311843a540f1c4c3fae44da151ae634a8651
wlandau <will.landau@gmail.com>
2016-05-24 14:11:59 -0400

    Modify clean_stages to handle more cases

--- R/clean_stages.R
+++ R/clean_stages.R
@@ -1,5 +1,6 @@
 check_stages = function(stages){
   if(!is.list(stages) | !length(stages)) stop("stages must be a nonempty list.")
+  if(is.null(names(stages))) stop("names(stages) must not be NULL.")
   if(any(!nchar(names(stages)))) stop("each element of names(stages) must be nonempty.")
   if(anyDuplicated(unlist(stages))) 
     stop("No duplicates allowed in unlist(stages) after .yml extensions are removed.")
