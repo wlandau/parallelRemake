689c0423b381500d89ea272e2f1da8464463491e
wlandau <will.landau@gmail.com>
2016-06-19 14:24:28 -0400

    Add tests to ensure yes and no can be commands

--- tests/testthat/test-yaml_yesno_truefalse.R
+++ tests/testthat/test-yaml_yesno_truefalse.R
@@ -3,7 +3,7 @@ context("yaml_yesno_truefalse")
 source("utils.R")
 
 test_that("Function yaml_yesno_truefalse is correct.", {
-  for(i in 1:2){
+  for(i in 1:3){
     x = readLines(paste0("test-yaml_yesno_truefalse/input", i, ".txt"))
     write(x, "out.txt")
     yaml_yesno_truefalse("out.txt")
