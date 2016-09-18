104584dd2f099caa2c8e20b31a38ec987bf77ecd
Will Landau <will.landau@gmail.com>
2016-09-17 22:07:47 -0400

    Add vignette

--- R/remake_utils.R
+++ R/remake_utils.R
@@ -6,31 +6,38 @@
 ## 
 ## LICENSE: https://raw.githubusercontent.com/richfitz/remake/master/LICENSE
 ##
-## The MIT License (MIT)
+## The BSD 2-Clause License
+##
+## Copyright (c) 2014, Rich FitzJohn 
+## (http://richfitz.github.io/, ## https://github.com/richfitz)
+## All rights reserved.
 ## 
-## Copyright (c) <2016> <Richard G. FitzJohn>
+## Redistribution and use in source and binary forms, with or 
+## without modification, are permitted provided that the following 
+## conditions are met:
 ## 
-## Permission is hereby granted, free of charge, to any person 
-## obtaining a copy of this software and associated documentation 
-## files (the "Software"), to deal in the Software without 
-## restriction, including without limitation the rights to use, 
-## copy, modify, merge, publish, distribute, sublicense, and/or
-## sell copies of the Software, and to permit persons to whom the 
-## Software is furnished to do so, subject to the following 
-## conditions:
+## 1. Redistributions of source code must retain the above 
+## copyright notice, this list of conditions and the following 
+## disclaimer.
 ## 
-## The above copyright notice and this permission notice shall be
-##  included in all copies or substantial portions of the
-##  Software.
+## 2. Redistributions in binary form must reproduce the above 
+## copyright notice, this list of conditions and the following 
+## disclaimer in the documentation and/or other materials provided 
+## with the distribution.
 ## 
-## THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
-## EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES 
-## OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
-## NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT 
-## HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
-## WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
-## FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR 
-## OTHER DEALINGS IN THE SOFTWARE.
+## THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND 
+## CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, 
+## INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF 
+## MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
+## DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR 
+## CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
+## SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT 
+## NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
+## LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) 
+## HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+## CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR 
+## OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, 
+## EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 assert_character <- function(x, name=deparse(substitute(x))) {
   if (!is.character(x)) {
@@ -150,31 +157,38 @@ yaml_load <- function(string) {
 #' @param filename Character, name of \code{YAML} file to read
 #' @details LICENSE: https://raw.githubusercontent.com/richfitz/remake/master/LICENSE
 #'
-#' The MIT License (MIT)
+#' The BSD 2-Clause License
 #' 
-#' Copyright (c) <2016> <Richard G. FitzJohn>
+#' Copyright (c) 2014, Rich FitzJohn 
+#' (http://richfitz.github.io/, #' https://github.com/richfitz)
+#' All rights reserved.
 #' 
-#' Permission is hereby granted, free of charge, to any person 
-#' obtaining a copy of this software and associated documentation 
-#' files (the "Software"), to deal in the Software without 
-#' restriction, including without limitation the rights to use, 
-#' copy, modify, merge, publish, distribute, sublicense, and/or
-#' sell copies of the Software, and to permit persons to whom the 
-#' Software is furnished to do so, subject to the following 
-#' conditions:
+#' Redistribution and use in source and binary forms, with or 
+#' without modification, are permitted provided that the following 
+#' conditions are met:
 #' 
-#' The above copyright notice and this permission notice shall be
-#'  included in all copies or substantial portions of the
-#'  Software.
+#' 1. Redistributions of source code must retain the above 
+#' copyright notice, this list of conditions and the following 
+#' disclaimer.
 #' 
-#' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
-#' EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES 
-#' OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
-#' NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT 
-#' HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
-#' WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
-#' FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR 
-#' OTHER DEALINGS IN THE SOFTWARE.
+#' 2. Redistributions in binary form must reproduce the above 
+#' copyright notice, this list of conditions and the following 
+#' disclaimer in the documentation and/or other materials provided 
+#' with the distribution.
+#' 
+#' THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND 
+#' CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, 
+#' INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF 
+#' MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
+#' DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR 
+#' CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
+#' SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT 
+#' NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
+#' LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) 
+#' HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+#' CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR 
+#' OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, 
+#' EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 yaml_read <- function(filename) {
   catch_yaml <- function(e) {
     stop(sprintf("while reading '%s'\n%s", filename, e$message),
