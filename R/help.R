469c0fb03b4815f7bfd318eca10ac3ca95898ea3
Will Landau <will.landau@gmail.com>
2016-11-19 09:51:55 -0500

    Ignore web files

--- R/help.R
+++ R/help.R
@@ -8,7 +8,7 @@ help_parallelRemake = function(){
 #    "    https://CRAN.R-project.org/package=parallelRemake/vignettes/parallelRemake.html",
 #    "    https://cran.r-project.org/web/packages/parallelRemake/vignettes/parallelRemake.html",
     "The vignette of the development version has a full tutorial at the webpage below.",
-    "    https://github.com/wlandau/parallelRemake/blob/master/vignettes/parallelRemake.Rmd",
+    "    http://will-landau.com/parallelRemake/articles/parallelRemake.html",
     "For troubleshooting, navigate to the link below.",
     "    https://github.com/wlandau/parallelRemake/blob/master/TROUBLESHOOTING.md",
     "To submit bug reports, usage questions, feature requests, etc., navigate to the link below.",
