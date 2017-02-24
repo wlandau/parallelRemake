72203481c25292313c49562bee76b2823fd449ed
Kirill Müller <krlmlr+r@mailbox.org>
2017-02-24 22:42:26 +0100

    proper support for implicit targets
    - important for plain data files that are not otherwise controlled by remake

--- R/makefile.R
+++ R/makefile.R
@@ -18,7 +18,7 @@ makefile_rules = function(remakefile, make_these, targets, add_args){
       next()
     }
     target = targets[[name]]
-    dep = unique(c(unlist(target$depends), parse_command(target$command)$depends))
+    dep = target$depends
     dep = dep[dep != "target_name"] # "target_name" is a keyword in remake.
     if (length(dep) > 0) {
       cat(paste0(timestamp(name), ": ", timestamp(dep)), sep = "\n")
@@ -77,6 +77,22 @@ makefile = function(targets = "all", remakefiles = "remake.yml",
   remakefile = collate_remakefiles(remakefiles)
   remake_data = yaml_read(remakefile)
   targets = remake_data$targets
+  targets = lapply(
+    targets,
+    function(target) {
+      target$depends = setdiff(
+        unique(c(
+          unlist(target$depends),
+          parse_command(target$command)$depends
+        )),
+        "target_name"
+      )
+      target
+    }
+  )
+  implicit_targets = setdiff(unlist(lapply(targets, "[[", "depends")), names(targets))
+  implicit_targets = stats::setNames(rep(list(NULL), length(implicit_targets)), implicit_targets)
+  targets <- c(targets, implicit_targets)
   stopifnot(all(make_these %in% names(targets)))
 
   sink("Makefile")
